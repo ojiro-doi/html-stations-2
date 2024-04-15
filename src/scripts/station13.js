865e8cb8afed4d9caacee835d730fe0311d52072
ojiro-doi <sakura.0046@icloud.com>
2024-04-15 17:32:02 +0900

    station13~15clear

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,9 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  const $text = document.getElementById("text");
+  if (check.checked === true) {
+    $text.style.backgroundColor = "red";
+  } else {
+    $text.style.backgroundColor = "white";
+  }
 }
---
+++ src/scripts/station15.js
@@ -0,0 +1,10 @@
+const displayList = () => {
+  const $list = document.getElementById("fruits");
+  $list.innerHTML = `
+    <ul>
+      <li>リンゴ</li>
+      <li>イチゴ</li>
+      <li>ブドウ</li>
+    </ul>
+    `;
+};
